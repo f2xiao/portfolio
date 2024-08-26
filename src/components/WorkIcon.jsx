ca83160a9deb05e86b713b857e72698a53551358
arashi4ever <25191617+f2xiao@users.noreply.github.com>
2024-08-26 14:11:12 -0400

    add icon to vertical timeline component

---
+++ src/components/WorkIcon.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import icon from "../assets/icons/coding.gif"
+
+const WorkIcon = () => {
+  return (
+    <div>
+        <img style={{width:"2.5rem", borderRadius:"50%"}} src={icon} alt="icon" />
+    </div>
+  )
+}
+
+export default WorkIcon
